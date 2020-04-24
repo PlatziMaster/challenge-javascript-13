5b554caaa620a2c96f5c3e4d5a355196ebf5d78d
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-04-24 08:11:21 -0500

    Challenge solved

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
-function print(a,b,c) {
-  return `${a}->${b}->${c}`;
-}
-
-function flip(fn) {
-  return function(){
-    // your code
-  }
-}
-
-const normalRta = print(1,2,3); // 1->2->3
-console.log(normalRta);
-const flipRta = flip(print)(1,2,3); // 3->2->1
-console.log(flipRta);
+/**
+ * flip arguments
+ * @param {Function} fn 
+ * @returns {Function}
+ */
+const flip = (fn) => (...arg) => fn(...arg.reverse());
 
 module.exports = { flip };
