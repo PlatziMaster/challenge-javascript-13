c2118dfcce6e57f48b7e1ef6247af0d78626798d
Jedp <jedp082@gmail.com>
2020-06-01 00:03:23 -0500

    Challenge done :D

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ function print(a,b,c) {
 
 function flip(fn) {
   return function(){
-    // your code
+    let flipElements = '';
+    for(let i = arguments.length-1; i >= 0; i--){
+      if(i === 0){
+        flipElements += `${arguments[i]}`;
+      }else{
+        flipElements += `${arguments[i]}->`;
+      }
+    }
+    return flipElements;
   }
 }
 
@@ -12,5 +20,4 @@ const normalRta = print(1,2,3); // 1->2->3
 console.log(normalRta);
 const flipRta = flip(print)(1,2,3); // 3->2->1
 console.log(flipRta);
-
 module.exports = { flip };
