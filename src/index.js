dfc2be08161a36a3127450ac542e7b2b675940b6
brahianf <brahianf@unicauca.edu.co>
2020-07-19 18:26:58 -0500

    Challenge Done

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ function print(a,b,c) {
 function flip(fn) {
   return function(){
     // your code
+    // El objeto arguments es un objeto similar a un Array que se corresponde con los argumentos pasados a la función.
+    return `${arguments[2]}->${arguments[1]}->${arguments[0]}`;
+    // const num = fn.split("->");
+    // const numReverse = num.reverse()
+    // return `${numReverse[0]}->${numReverse[1]}->${numReverse[2]}`
   }
 }
 
