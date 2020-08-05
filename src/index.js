2312e93b8f0a89cbb4a5c2e24117995413405c84
Max23Esau <esa-max23@live.com>
2020-08-04 19:45:02 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-function print(a,b,c) {
+function print(a, b, c) {
   return `${a}->${b}->${c}`;
 }
 
 function flip(fn) {
-  return function(){
-    // your code
-  }
+  return (a, b, c) => fn(c, b, a)
 }
 
-const normalRta = print(1,2,3); // 1->2->3
+const normalRta = print(1, 2, 3); // 1->2->3
 console.log(normalRta);
-const flipRta = flip(print)(1,2,3); // 3->2->1
+const flipRta = flip(print)(1, 2, 3); // 3->2->1
 console.log(flipRta);
 
 module.exports = { flip };
