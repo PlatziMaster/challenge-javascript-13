6cf3db16d659344f6f6471a5076a42d94e302763
frxncismor <frxncismor@gmail.com>
2020-11-24 14:39:18 -0600

    Resolved

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-function print(a,b,c) {
-  return `${a}->${b}->${c}`;
+function print(a, b, c) {
+	return `${a}->${b}->${c}`;
 }
 
 function flip(fn) {
-  return function(){
-    // your code
-  }
+	return function (...params) {
+		return fn(...params.reverse());
+	};
 }
 
-const normalRta = print(1,2,3); // 1->2->3
+const normalRta = print(1, 2, 3); // 1->2->3
 console.log(normalRta);
-const flipRta = flip(print)(1,2,3); // 3->2->1
+const flipRta = flip(print)(1, 2, 3); // 3->2->1
 console.log(flipRta);
 
-module.exports = { flip };
+module.exports = {flip};
